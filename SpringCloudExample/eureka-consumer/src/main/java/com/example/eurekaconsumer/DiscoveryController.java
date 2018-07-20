a0b15dc1c78404693aa062fef96a1ef81041ff6d
Lei-Mac <1793161563@qq.com>
2018-07-20 09:35:33 +0800

    add eureka example

---
+++ SpringCloudExample/eureka-consumer/src/main/java/com/example/eurekaconsumer/DiscoveryController.java
@@ -0,0 +1,26 @@
+package com.example.eurekaconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DiscoveryController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+}
